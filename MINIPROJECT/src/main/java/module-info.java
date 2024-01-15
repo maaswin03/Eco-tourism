65559f840b045ae32e17ed20373067a138f7631d
ASWIN <123287737+maaswin03@users.noreply.github.com>
2024-01-15 08:09:14 +0530

    Delete MINIPROJECT directory

--- MINIPROJECT/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.miniproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires mongo.java.driver;
-
-
-    opens com.example.miniproject to javafx.fxml;
-    exports com.example.miniproject;
-}
